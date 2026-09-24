b7871081d3efb9f44c8db4b2858720aeadf3b901
Z1p_pL <68015225+Z1ppL@users.noreply.github.com>
2026-09-25 04:43:05 +0500

    Delete main.tsx

--- main.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import Demo from './app/demo';
-import './app/globals.css';
-const page=location.pathname.replace(/^\//,'').replace(/\/$/,'');
-createRoot(document.getElementById('root')!).render(<Demo page={(['session','wheel','topup','admin'].includes(page)?page:'session') as 'session'|'wheel'|'topup'|'admin'}/>);
